df047213db64f201e3adf3990585bfccdfabe132
Eldelshell <alejandroayuso@gmail.com>
2014-09-05 12:17:21 +0200

    New Localizable interface for elements that want to be localized

---
+++ jobhunter/src/main/java/jobhunter/gui/Localizable.java
@@ -0,0 +1,19 @@
+package jobhunter.gui;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public interface Localizable {
+
+	public ResourceBundle getBundle();
+	
+	default String getTranslation(String str) {
+		return getBundle().getString(str);
+	}
+	
+	default String getTranslation(String str, Object...objects) {
+		final String i18n = getBundle().getString(str);
+		return MessageFormat.format(i18n, objects);
+	}
+	
+}
