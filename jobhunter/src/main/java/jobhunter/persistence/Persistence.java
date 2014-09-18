81ab2494e0b29afdeda83f5ba9a152c3b6a99143
Eldelshell <alejandroayuso@gmail.com>
2014-09-18 13:11:33 +0200

    Set and verify the last modification time and MD5 checksum so we can detect changes before saving.

--- jobhunter/src/main/java/jobhunter/persistence/Persistence.java
+++ jobhunter/src/main/java/jobhunter/persistence/Persistence.java
@@ -17,8 +17,14 @@
 package jobhunter.persistence;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 import java.util.Optional;
 import java.util.zip.ZipEntry;
@@ -29,47 +35,58 @@ import jobhunter.models.Profile;
 import jobhunter.models.Subscription;
 import jobhunter.utils.ApplicationState;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.thoughtworks.xstream.XStream;
 
-public final class Persistence {
+/**
+ * Singleton for persistence operations.
+ */
+public enum Persistence {
+	
+	_INSTANCE;
 	
 	private static final Logger l = LoggerFactory.getLogger(Persistence.class);
-	private static final XStream xstream = new XStream();
+	private final XStream xstream = new XStream();
 	
-	static {
+	private Long lastModification;
+	private byte[] md5sum;
+	
+	private Persistence() {
 		xstream.registerConverter(new LocalDateTimeConverter());
 		xstream.registerConverter(new ObjectIdConverter());
 		xstream.registerConverter(new LocalDateConverter());
 	}
 	
-	public static void save(final File file) {
+	@SuppressWarnings("unchecked")
+    static <T> List<T> cast(Object obj) {
+        return (List<T>)obj;
+    }
+	
+	private void _save(final File file) {
+		if(wasModified(file))
+			throw new ConcurrentModificationException("File has been modified");
+
 		ApplicationState.instanceOf().changesPending(false);
 		zip(file);
 	}
 	
-	public static Optional<Profile> readProfile(final File file) {
+	private Optional<Profile> _readProfile(final File file) {
 		
 		try(ZipFile zfile = new ZipFile(file)) {
 			l.debug("Reading profile from JHF File");
-			final Object obj = xstream.fromXML(zfile.getInputStream(new ZipEntry("profile.xml")));
+			final InputStream in = zfile.getInputStream(new ZipEntry("profile.xml"));
+			
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			DigestInputStream dis = new DigestInputStream(in, md);
+			
+			final Object obj = xstream.fromXML(dis);
+			
+			updateLastMod(file, md);
+			
 			return Optional.of((Profile)obj);
-		}catch(IOException | ClassCastException e){
-			l.error("Failed to read file: {}", e.getMessage());
-		}
-		
-		return Optional.empty();
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static Optional<List<Subscription>> readSubscriptions(final File file) {
-		
-		try(ZipFile zfile = new ZipFile(file)) {
-			l.debug("Reading subscriptions from JHF File");
-			final Object obj = xstream.fromXML(zfile.getInputStream(new ZipEntry("subscriptions.xml")));
-			return Optional.of((List<Subscription>)obj);
 		}catch(Exception e){
 			l.error("Failed to read file: {}", e.getMessage());
 		}
@@ -77,7 +94,30 @@ public final class Persistence {
 		return Optional.empty();
 	}
 	
-	private static void zip(final File file) {
+	
+	private Optional<List<Subscription>> _readSubscriptions(final File file) {
+		
+		try(ZipFile zfile = new ZipFile(file)) {
+			l.debug("Reading subscriptions from JHF File");
+			final InputStream in = zfile.getInputStream(new ZipEntry("subscriptions.xml"));
+			
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			DigestInputStream dis = new DigestInputStream(in, md);
+			
+			final Object obj = xstream.fromXML(dis);
+			
+			updateLastMod(file, md);
+			
+			return Optional.of(cast(obj));
+		}catch(Exception e){
+			l.error("Failed to read file: {}", e.getMessage());
+		}
+		
+		return Optional.empty();
+	}
+	
+	private void zip(final File file) {
+		
 		try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(file))){
 			l.debug("Generating JHF file");
 			zout.setMethod(ZipOutputStream.DEFLATED);
@@ -91,18 +131,66 @@ public final class Persistence {
 			zout.putNextEntry(new ZipEntry("subscriptions.xml"));
 			xstream.toXML(SubscriptionRepository.instanceOf().getSubscriptions(), zout);
 			
+			updateLastMod(file);
 		}catch(IOException e){
 			l.error("Failed to generate file", e);
 		}
 		
 	}
 	
+	private void updateLastMod(final File file) {
+		this.lastModification = file.lastModified();
+		try(InputStream in = new FileInputStream(file)){
+			this.md5sum = DigestUtils.md5(in);
+		} catch (IOException e) {
+			l.error("Failed to read MD5 checksum from {}", file.toString(), e);
+		}
+		l.debug("File was last modified on {} with MD5 {}", lastModification, this.md5sum.toString());
+	}
+	
+	@SuppressWarnings("unused")
+	private void updateLastMod(final File file, final InputStream in) throws IOException {
+		this.lastModification = file.lastModified();
+		this.md5sum = DigestUtils.md5(in);
+	}
+	
+	private void updateLastMod(final File file, final MessageDigest md) throws IOException {
+		this.lastModification = file.lastModified();
+		this.md5sum = md.digest();
+		l.debug("File was last modified on {} with MD5 {}", lastModification, this.md5sum.toString());
+	}
+	
+	private Boolean wasModified(final File file) {
+		if(this.lastModification != file.lastModified())
+			return true;
+		
+		try(InputStream in = new FileInputStream(file)){
+			return Arrays.equals(this.md5sum, DigestUtils.md5(in));
+		} catch (IOException e) {
+			l.error("Failed to read MD5 checksum from {}", file.toString(), e);
+		}
+		
+		return false;
+	}
+	
+	public static void save(final File file) {
+		_INSTANCE._save(file);
+	}
+	
+	public static Optional<Profile> readProfile(final File file) {
+		return _INSTANCE._readProfile(file);
+	}
+	
+	public static Optional<List<Subscription>> readSubscriptions(final File file) {
+		return _INSTANCE._readSubscriptions(file);
+	}
+	
 	public static String debugProfile() {
-		return xstream.toXML(ProfileRepository.instanceOf().getProfile());
+		return _INSTANCE.xstream.toXML(ProfileRepository.instanceOf().getProfile());
 	}
 	
 	public static String debugSubscriptions() {
-		return xstream.toXML(SubscriptionRepository.instanceOf().getSubscriptions());
+		return _INSTANCE.xstream.toXML(SubscriptionRepository.instanceOf().getSubscriptions());
 	}
 	
 }
